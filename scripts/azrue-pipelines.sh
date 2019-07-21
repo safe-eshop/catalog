b0d02da66b5bf0292716a587a0996c6c687c2302
dominikus1993 <dominikus1910@gmail.com>
2019-07-21 20:45:15 +0200

    fix build error

--- scripts/azrue-pipelines.sh
+++ scripts/azrue-pipelines.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod -R a+x build
-cd ./build
+chmod -R a+x scripts
+cd ./scripts
 rm -rf .fake packages paket-files/paket.restore.cached paket.lock build.fsx.lock
 ./fake.sh run build.fsx --target $1
