2004d9714f76ffdb22483fb1cba42379def2dba9
dominikus1993 <dominikus1910@gmail.com>
2019-09-14 19:15:22 +0000

    init rust project

--- scripts/azrue-pipelines.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chmod -R a+x scripts
-cd ./scripts
-rm -rf .fake packages paket-files/paket.restore.cached paket.lock build.fsx.lock
-./fake.sh run build.fsx --target $1
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let a = 2;
+    let b = a * a;
+    println!("Hello, world! {} ", b);
+}
