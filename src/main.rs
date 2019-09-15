677a31fbcb0e7e3e9da65fba659639ded0f288f9
dominikus1993 <dominikus1910@gmail.com>
2019-09-15 21:29:43 +0000

    small changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use cpuprofiler::PROFILER;
+
+#[derive(Debug)]
+struct Product {
+    id: String,
+    q: i32
+}
 fn main() {
-    let a = 2;
-    let b = a * a;
-    println!("Hello, world! {} ", b);
+    PROFILER.lock().unwrap().start("./my-prof.profile");
+    let mut a = Vec::new();
+    a.push(Product{ id: String::from("hasdhkjdsa"), q: 3});
+    a.push(Product{ id: String::from("hasdhkjdsa"), q: 4});
+    let b = a.iter().map(|x| x.q).filter(|&x| x == 3).collect::<Vec<_>>();
+    PROFILER.lock().unwrap().stop();
+    println!("Hello, world! {:?}", b);
 }
