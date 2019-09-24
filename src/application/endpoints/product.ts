d4140babba7b67bb2e2ea8b6c6c2c1282ef942dd
dominikus1993 <dominikus1910@gmail.com>
2019-09-24 20:23:25 +0000

    modeling

--- src/application/endpoints/product.ts
+++ src/application/endpoints/product.ts
@@ -1,9 +1,13 @@
 import {FastifyInstance} from "fastify"
 import { Server, IncomingMessage, ServerResponse } from "http"
+import { IProductService } from "../../domain/service/product"
+import { ProductService } from "../../infrastructure/service/product"
+import { PostgresProductRepository } from "../../infrastructure/repository/product"
 
 export default async function routes(fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>, options: any) {
     fastify.get('/', async (request, reply) => {
-        return { hello: 'world' }
+        const service: IProductService = new ProductService(new PostgresProductRepository())
+        return service.getAll()
       })
 }
 
--- src/infrastructure/repository/product.ts
+++ src/infrastructure/repository/product.ts
@@ -1,11 +1,11 @@
 import { IProductRepository, } from "../../domain/repository/product";
 import { Product } from "../../domain/model/product"
 export class PostgresProductRepository implements IProductRepository {
-    getAll(): Promise<Product[] | null> {
-        throw new Error("Method not implemented.");
+    async getAll(): Promise<Product[] | null> {
+        return [{ id: "spierdalaj" }];
     }
-    get(id: string): Promise<Product | null> {
-        throw new Error("Method not implemented.");
+    async get(id: string): Promise<Product | null> {
+        return { id: "spierdalaj" }
     }
 
 }
