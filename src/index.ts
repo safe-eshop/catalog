15099913b2aeeea3a3ab9b72f2616c2302519b69
dominikus1993 <dominikus1910@gmail.com>
2019-11-18 22:59:36 +0100

    add koa.js

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-// Require the framework and instantiate it
-import fastify from "fastify"
+import Koa from "koa"
+import Router from "koa-router"
+import json from "koa-json"
+import logger from "koa-logger"
 
-const server = fastify({logger: true})
-server.register(require("./api/endpoints/product.ts"))
-// Run the server!
-const start = async () => {
-    try {
-        await server.listen((process.env.PORT || 3000) as number)
+const app = new Koa()
+const router = new Router()
 
-        server.log.info(`server listening on ${server.server.address()}`)
-    } catch (err) {
-        server.log.error(err)
-        process.exit(1)
-    }
-}
-start();
+router.get("/", async (ctx, next) => {
+    ctx.body = { message: "Test"}
+    await next();
+})
+
+
+app.use(json())
+app.use(logger())
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+    console.log(`Server listening on port ${3000}`);
+})
