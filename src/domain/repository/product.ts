e4f4dfc9c2a25f1c5846f1a083bb8a906edfee5e
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2020-09-14 10:06:38 +0200

    remove nodejs

--- src/domain/repository/product.ts
+++
@@ -1,8 +0,0 @@
-import {Product, ProductId} from "../model/product";
-import {Option} from "fp-ts/lib/Option";
-
-export interface IProductRepository {
-    get(id: ProductId): Promise<Option<Product>>
-    count() : Promise<number>
-    insertMany(products: Product[]): Promise<void>;
-}
