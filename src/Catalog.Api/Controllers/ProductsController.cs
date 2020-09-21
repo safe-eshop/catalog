3792f39eed981fa28aa50581f3777b255d974bf4
dominikus1993 <dominikus1910@gmail.com>
2020-09-21 12:32:04 +0200

    add ProductsController.cs

---
+++ src/Catalog.Api/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Catalog.Api.Framework.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController: ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductResponse>> GetProductById()
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ src/Catalog.Api/Framework/Responses/ProductResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Catalog.Api.Framework.Responses
+{
+    public class ProductResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
