29141926dd8b26b15aaf853de7664ddb58944671
Dominik.Kotecki <dominik.kotecki@rossmann.com.pl>
2021-08-20 14:19:45 +0000

    add ProductCreated

--- core/services/messaging.go
+++ core/services/messaging.go
@@ -2,14 +2,21 @@ package services
 
 import (
 	"catalog/core/dto"
+	"catalog/core/model"
 	"context"
 )
 
 type ProductCreated struct {
+	ID             model.ProductId `json:"id,omitempty"`
+	Name           string          `json:"name,omitempty"`
+	Brand          string          `json:"brand,omitempty"`
+	Description    string          `json:"description,omitempty"`
+	Price          float64         `json:"price,omitempty"`
+	PromotionPrice *float64        `json:"promotionPrice,omitempty"`
 }
 
 func NewProductCreated(dto dto.ProductDto) ProductCreated {
-	return ProductCreated{}
+	return ProductCreated{ID: dto.ID, Name: dto.Name, Brand: dto.Brand, Description: dto.Description, Price: dto.Price, PromotionPrice: dto.PromotionPrice}
 }
 
 type MessageBus interface {
